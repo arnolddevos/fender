c210bd554d5ea0e89b10add5b7d64748d7b9cfc6
Arnold deVos <adv@langdale.com.au>
2017-03-29 16:46:36 +1100

    add async http get

--- src/main/scala/fender/Clients.scala
+++ src/main/scala/fender/Clients.scala
@@ -1,9 +1,38 @@
 package fender
 
 import org.eclipse.jetty
-import jetty.client.{HttpClient}
+import jetty.client._
+import jetty.client.api._
+import jetty.client.util._
+import flowlib._
+import Process._
 
 trait Clients { this: Builders with Containers =>
   def client = build { new HttpClient  }
   def followRedirects = assign[HttpClient, Boolean] {_ setFollowRedirects _}
+
+  def sendGET(url: String, limit: Int)(implicit pool: HttpClient): Process[String] = {
+
+    def fetch: Process[Process[String]] = waitFor {
+      k =>
+
+        val l = new BufferingResponseListener(limit) {
+          override def onSuccess(r: Response): Unit = {
+            if(r.getStatus == 200)
+              k(stop(getContentAsString()))
+            else
+              k(fail("taglist not available: " + r.getReason))
+          }
+          override def onFailure(r: Response, e: Throwable): Unit = {
+            k(fail("taglist not available", e))
+          }
+          override def onComplete(r: Result): Unit = {}
+        }
+
+        pool.newRequest(url).send(l)
+
+    }
+
+    fetch >>= identity
+  }
 }
